246533810ab6a57a053a60d7a64066dd248eac4f
dlwjddms <38257475+dlwjddms@users.noreply.github.com>
2021-03-23 17:10:00 -0700

    Create 정수삼각형.cpp

---
+++ level3/정수삼각형.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    
+    for(int i=triangle.size()-1; i>0;i--){
+        for(int j=0; j<triangle[i].size()-1;j++){
+            if(triangle[i][j]>triangle[i][j+1]){
+                triangle[i-1][j] +=triangle[i][j];
+            }else
+                triangle[i-1][j] +=triangle[i][j+1];
+        }
+    }
+    return triangle[0][0];
+}
